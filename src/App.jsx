d0ad18d19bf7a4aed69783d4e764e5e45a372fff
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-08-22 08:44:37 +0700

    ❌ delete comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import Home from "./page/Home";
-import TakeNotes from "./page/TakeNotes";
-import Contact from "./page/Contact";
-import NotFound from "./page/NotFound";
+import Home from "./pages/Home";
+import TakeNotes from "./pages/TakeNotes";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/takenotes" element={<TakeNotes/>} />
-          <Route path="/contact" element={<Contact/>} />
-        </Routes>
-      </BrowserRouter> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/takenotes" element={<TakeNotes />} />
