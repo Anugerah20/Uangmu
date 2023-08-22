28c74685c800362076c428d4895b0daa8c0167c3
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-08-22 12:57:06 +0700

    Add modal for take note

--- src/pages/TakeNotes.jsx
+++ src/pages/TakeNotes.jsx
@@ -1,174 +1,186 @@
+import React, { useState, useRef } from 'react';
 import { Button, Label, TextInput } from 'flowbite-react';
-import { Table } from 'flowbite-react';
+import { Modal, Table } from 'flowbite-react';
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { useState } from 'react';
 
 const TakeNotes = () => {
-     document.title = "Uangmu | Buat Catatan";
-
-     // Inisialisasi data yg mau disimpan
      const [deskripsi, setDeskripsi] = useState("");
      const [tanggal, setTanggal] = useState("");
      const [nominal, setNominal] = useState("");
      const [savedData, setSavedData] = useState([]);
+     const [openModal, setOpenModal] = useState(false);
+     const setModal = useRef(null);
 
-     // Menambahkan titik dibagian nominal
+     // State Input
+     const handlerInputChange = (e, inputName) => {
+          const newValue = e.target.value;
+          switch (inputName) {
+               case "deskripsi":
+                    setDeskripsi(newValue);
+                    break;
+               case "tanggal":
+                    setTanggal(newValue);
+                    break;
+               case "nominal":
+                    setNominal(newValue);
+                    break;
+               default:
+                    break;
+          }
+     };
 
-     function dataNote(e) {
+     // Validation input
+     const handleSubmit = (e) => {
           e.preventDefault();
 
-          // Validasi inputan
-          if (deskripsi === "") {
-               return alert("Deskripsi tidak boleh kosong");
-          } else if (tanggal === "") {
-               return alert("Tanggal tidak boleh kosong");
-          } else if (nominal === "") {
-               return alert("Nominal tidak boleh kosong");
-          } else if (isNaN(nominal) === true) {
+          if (deskripsi === "" || tanggal === "" || nominal === "") {
+               alert("Harus diisi semua");
+               return;
+          } else if (isNaN(nominal)) {
                return alert("Nominal hanya menerima angka");
           }
 
-          // Wadah untuk menyimpan data sementara
+          // Storage
           const newDataNote = {
                deskripsi,
                tanggal,
                nominal
           };
-          console.log(newDataNote);
 
           const updatedData = [...savedData, newDataNote];
           setSavedData(updatedData);
 
-          // clear input
+          // Clear input
           setDeskripsi("");
           setTanggal("");
           setNominal("");
-     }
+          setOpenModal(false);
+     };
 
-     // function untuk tabel
-     function DataNoteTable({ deskripsi, tanggal, nominal }) {
-          const formatAmountID = Number(nominal).toLocaleString("id-ID");
+     // Function table
+     const DataNoteTable = ({ deskripsi, tanggal, nominal }) => {
+          const formatAmountID = new Intl.NumberFormat("id-ID").format(nominal);
 
           const partDate = tanggal.split("-");
           const formattedDate = `${partDate[2]}/${partDate[1]}/${partDate[0]}`;
           return (
                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                    <Table.Cell className="whitespace-nowrap">
-                         {deskripsi}
-                    </Table.Cell>
-                    <Table.Cell>
-                         {formattedDate}
-                    </Table.Cell>
-                    <Table.Cell>
-                         Rp {formatAmountID}
-                    </Table.Cell>
+                    <Table.Cell className="whitespace-nowrap">{deskripsi}</Table.Cell>
+                    <Table.Cell>{formattedDate}</Table.Cell>
+                    <Table.Cell>Rp {formatAmountID}</Table.Cell>
                </Table.Row>
-          )
-     }
+          );
+     };
 
      return (
           <>
                <Navigation />
 
-               <h1 className="text-2xl font-bold text-center">Buat Catatan</h1>
+               <div className="flex flex-wrap">
 
-               {/* START: FORMULIR KEUANGAN */}
-               <form onSubmit={dataNote} className="flex max-w-md flex-col gap-4 mx-auto">
-                    <div>
-                         <div className="mb-2 block">
-                              <Label
-                                   htmlFor="deskripsi"
-                                   value="Deskripsi"
-                              />
-                         </div>
-                         <TextInput
-                              id="deskripsi"
-                              name="deskripsi"
-                              placeholder="contoh: beli kopi"
-                              type="text"
-                              autoComplete="off"
-                              value={deskripsi}
-                              onChange={(e) => setDeskripsi(e.target.value)}
-                         />
+                    {/* START: MODAL KEUANGAN */}
+                    <div className="flex flex-col gap-4 mx-auto w-4/5 sm:w-2/4 md:w-2/5">
+                         <Button onClick={() => setOpenModal(true)}>Buat Catatan Pemasukan</Button>
+                         <Modal
+                              show={openModal}
+                              size="md"
+                              popup
+                              onClose={() => setOpenModal(false)}
+                              initialFocus={setModal}
+                         >
+                              <Modal.Header />
+                              <Modal.Body>
+                                   <div className="space-y-6">
+                                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">Tambah Catatan Keuangan</h3>
+                                        <div>
+                                             <div className="mb-2 block">
+                                                  <Label htmlFor="deskripsi" value="Deskripsi" />
+                                             </div>
+                                             <TextInput
+                                                  id="deskripsi"
+                                                  name="deskripsi"
+                                                  placeholder="contoh: beli kopi"
+                                                  type="text"
+                                                  autoComplete="off"
+                                                  value={deskripsi}
+                                                  onChange={(e) => handlerInputChange(e, "deskripsi")}
+                                             />
+                                        </div>
+                                        <div>
+                                             <div className="mb-2 block">
+                                                  <Label htmlFor="tanggal" value="Tanggal" />
+                                             </div>
+                                             <TextInput
+                                                  id="tanggal"
+                                                  name="tanggal"
+                                                  type="date"
+                                                  value={tanggal}
+                                                  onChange={(e) => handlerInputChange(e, "tanggal")}
+                                             />
+                                        </div>
+                                        <div>
+                                             <div className="mb-2 block">
+                                                  <Label htmlFor="nominal" value="Nominal" />
+                                             </div>
+                                             <TextInput
+                                                  id="nominal"
+                                                  name="nominal"
+                                                  placeholder="contoh: 10000"
+                                                  type="text"
+                                                  autoComplete="off"
+                                                  value={nominal}
+                                                  onChange={(e) => handlerInputChange(e, "nominal")}
+                                             />
+                                        </div>
+                                        <Button type="submit" onClick={handleSubmit}>Kirim Sekarang</Button>
+                                   </div>
+                              </Modal.Body>
+                         </Modal>
                     </div>
-                    <div>
-                         <div className="mb-2 block">
-                              <Label
-                                   htmlFor="tanggal"
-                                   value="Tanggal"
-                              />
-                         </div>
-                         <TextInput
-                              id="tanggal"
-                              name="tanggal"
-                              type="date"
-                              value={tanggal}
-                              onChange={(e) => setTanggal(e.target.value)}
-                         />
-                    </div>
-                    <div>
-                         <div className="mb-2 block">
-                              <Label
-                                   htmlFor="nominal"
-                                   value="Nominal"
-                              />
-                         </div>
-                         <TextInput
-                              id="nominal"
-                              name="nominal"
-                              placeholder="contoh: 10000"
-                              type="text"
-                              autoComplete="off"
-                              value={nominal}
-                              onChange={(e) => setNominal(e.target.value)}
-                         />
-                    </div>
-                    <Button type="submit">
-                         Kirim
-                    </Button>
-               </form>
-               {/* END: FORMULIR KEUANGAN */}
+                    {/* END: MODAL KEUANGAN */}
 
-               {/* START: Memasukkan Data */}
-               <div className="flex max-w-md flex-col mx-auto mt-10 mb-5">
-                    <Table>
-                         <Table.Head>
-                              <Table.HeadCell>
-                                   Deskripsi
-                              </Table.HeadCell>
-                              <Table.HeadCell>
-                                   Tanggal
-                              </Table.HeadCell>
-                              <Table.HeadCell>
-                                   Nominal
-                              </Table.HeadCell>
-                         </Table.Head>
-                         <Table.Body className="divide-y">
-                              {savedData.length <= 0 ? (
-                                   <tr>
-                                        <td colSpan="3" className="text-center font-bold py-2">
-                                             Data kosong!
-                                        </td>
-                                   </tr>
-                              ) : (
-                                   savedData.map((data, index) => (
-                                        <DataNoteTable
-                                             key={index}
-                                             deskripsi={data.deskripsi}
-                                             tanggal={data.tanggal}
-                                             nominal={data.nominal}
-                                        />
-                                   ))
-                              )}
-                         </Table.Body>
-                    </Table>
+                    {/* START: MEMASUKKAN DATA */}
+                    <div className="flex flex-col mx-auto mt-10 mb-5 w-4/5 sm:w-2/4 md:w-2/5">
+
+                         <Table className="text-center">
+                              <Table.Head>
+                                   <Table.HeadCell>
+                                        Deskripsi
+                                   </Table.HeadCell>
+                                   <Table.HeadCell>
+                                        Tanggal
+                                   </Table.HeadCell>
+                                   <Table.HeadCell>
+                                        Nominal
+                                   </Table.HeadCell>
+                              </Table.Head>
+                              <Table.Body className="divide-y">
+                                   {savedData.length === 0 ? (
+                                        <tr>
+                                             <td colSpan="3" className="text-center font-bold py-2">
+                                                  Data kosong!
+                                             </td>
+                                        </tr>
+                                   ) : (
+                                        savedData.map((data, index) => (
+                                             <DataNoteTable
+                                                  key={index}
+                                                  deskripsi={data.deskripsi}
+                                                  tanggal={data.tanggal}
+                                                  nominal={data.nominal}
+                                             />
+                                        ))
+                                   )}
+                              </Table.Body>
+                         </Table>
+                    </div>
+                    {/* END: MEMASUKKAN DATA */}
+
                </div>
-
-               {/* END: Memasukkan Data */}
                <Footer />
           </>
-     )
-}
+     );
+};
 
 export default TakeNotes;
