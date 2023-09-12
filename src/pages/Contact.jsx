7d7b41f1246b3ff1a6a57c7c42813ee46ab03b0d
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-09-12 09:47:44 +0700

    updated contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,13 +6,17 @@ const Contact = () => {
      return (
           <>
                <Navigation />
-               <h1 className="text-2xl font-bold text-center">Kontak</h1>
+               <h1 className="text-2xl font-bold text-center">Kirim Pesanmu Disini</h1>
                {/* START: KONTAK */}
-               <section className="text-center mt-8">
+
+               {/* <section className="text-center mt-8">
                     <a href="https://github.com/Anugerah20" target="_blank" className="py-2 px-4 bg-black text-white rounded-md me-3">✔️ Github</a>
                     <a href="https://web.facebook.com/nabil.pangestu.359/" target="_blank" className="py-2 px-4 bg-black text-white rounded-md me-3">✔️ Facebook</a>
                     <a href="https://instagram.com/anugerah" target="_blank" className="py-2 px-4 bg-black text-white rounded-md me-3">✔️ Instagram</a>
-               </section>
+               </section> */}
+
+
+
                {/* END: KONTAK */}
                <Footer />
           </>
