ed91c8d76e5e01e954ab0e6d5f27d6fbd8bfd547
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-09-23 19:39:59 +0700

    add link navigate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,23 +32,23 @@ import { Footer } from "flowbite-react";
 export default function MyFooter() {
   return (
     <div className="mx-5 sm:mx-20">
-    <Footer container className="flex justify-between sm:justify-between">
-      <Footer.Copyright
-        by="Uangmu"
-        href="/"
-      />
-      <Footer.LinkGroup className="flex justify-end sm:justify-around">
-        <Footer.Link href="#" className="px-1 underline">
-          Beranda
-        </Footer.Link>
-        <Footer.Link href="#" className="px-1 underline">
-        Buat Catatan
-        </Footer.Link>
-        <Footer.Link href="#" className="px-1 underline">
-          Kontak
-        </Footer.Link>
-      </Footer.LinkGroup>
-    </Footer>
+      <Footer container className="flex justify-between sm:justify-between">
+        <Footer.Copyright
+          by="Uangmu"
+          href="/"
+        />
+        <Footer.LinkGroup className="flex justify-end sm:justify-around">
+          <Footer.Link href="/" className="px-1 underline">
+            Beranda
+          </Footer.Link>
+          <Footer.Link href="/takenotes" className="px-1 underline">
+            Buat Catatan
+          </Footer.Link>
+          <Footer.Link href="/contact" className="px-1 underline">
+            Kontak
+          </Footer.Link>
+        </Footer.LinkGroup>
+      </Footer>
     </div>
   )
 }
