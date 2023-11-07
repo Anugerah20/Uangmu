3a646fb3e6b859932d36a18877168bffe8d2e88f
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-11-07 09:12:42 +0700

    Add feature filter month

--- src/components/TotalAmout.jsx
+++ src/components/TotalAmout.jsx
@@ -1,22 +1,56 @@
 import { Card } from "flowbite-react";
+import { useState, useEffect } from "react";
 
-const TotalAmout = ({ totalUang, totalPemasukan, totalPengeluaran }) => {
+const TotalAmout = ({ totalUang, totalPemasukan, totalPengeluaran, selectMonth, savedData }) => {
+     const [filterTotalPemasukan, setFilterTotalPemasukan] = useState(totalPemasukan);
+     const [filterTotalPengeluaran, setFilterTotalPengeluaran] = useState(totalPengeluaran);
+
      const numberFormatAmount = (number) => {
           return new Intl.NumberFormat("id-ID").format(number);
      }
+
+     useEffect(() => {
+          // Calculate pemasukan & pengeluaran
+          const filterPemasukan = savedData
+               .filter((data) => {
+                    return (
+                         (selectMonth === 'Semua Bulan' ||
+                              new Date(data.tanggal).toLocaleString('id-ID', { month: 'long' }) === selectMonth) &&
+                         data.jenis === 'pemasukan'
+                    );
+               })
+               .reduce((total, data) => total + data.nominal, 0);
+
+          const filterPengeluaran = savedData
+               .filter((data) => {
+                    return (
+                         (selectMonth === 'Semua Bulan' ||
+                              new Date(data.tanggal).toLocaleString('id-ID', { month: 'long' }) === selectMonth) &&
+                         data.jenis === 'pengeluaran'
+                    );
+               })
+               .reduce((total, data) => total + data.nominal, 0);
+
+          setFilterTotalPemasukan(filterPemasukan);
+          setFilterTotalPengeluaran(filterPengeluaran);
+
+     }, [savedData, selectMonth])
+     // Filter total uang
+     const filterTotalUang = filterTotalPemasukan - filterTotalPengeluaran;
+
      return (
           <Card className="w-4/5 sm:w-1/2 md:w-2/5 card-note">
                <h1 className="text-center text-2xl">Total Uang Sekarang</h1>
                <hr />
-               <h2 className="text-center text-xl font-bold">Rp {totalUang !== 0 ? numberFormatAmount(totalUang) : 0}</h2>
+               <h2 className="text-center text-xl font-bold">Rp {filterTotalUang !== 0 ? numberFormatAmount(totalUang) : 0}</h2>
                <section className="flex flex-wrap justify-between">
                     <div className="income">
                          <h5>🟢Income</h5>
-                         <small className="ml-5 font-bold">Rp {numberFormatAmount(totalPemasukan)}</small>
+                         <small className="ml-5 font-bold">Rp {numberFormatAmount(filterTotalPemasukan)}</small>
                     </div>
                     <div className="expenses">
                          <h5>🔴Expenses</h5>
-                         <small className="ml-5 font-bold">Rp {numberFormatAmount(totalPengeluaran)}</small>
+                         <small className="ml-5 font-bold">Rp {numberFormatAmount(filterTotalPengeluaran)}</small>
                     </div>
                </section>
           </Card>
