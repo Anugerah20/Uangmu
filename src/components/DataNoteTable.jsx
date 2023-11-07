c6b75009631d1d685500848c0cd4e4a187d2f8bc
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-11-07 09:11:56 +0700

    Make padding table

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -13,11 +13,11 @@ const DataNoteTable = ({ savedData, handleDelete, handleEdit }) => {
      return (
           <>
                <tr className="bg-primary border-gray-300">
-                    <td className="whitespace-nowrap">{savedData.deskripsi}</td>
-                    <td>{formattedDate}</td>
-                    <td>{formatAmountID}</td>
-                    <td>{savedData.jenis === "pemasukan" ? "pemasukan" : "pengeluaran"}</td>
-                    <td className="flex justify-center gap-2 mt-1">
+                    <td className="whitespace-nowrap px-6 py-3">{savedData.deskripsi}</td>
+                    <td className="px-6 py-3">{formattedDate}</td>
+                    <td className="px-6 py-3">{formatAmountID}</td>
+                    <td className="px-6 py-3">{savedData.jenis === "pemasukan" ? "pemasukan" : "pengeluaran"}</td>
+                    <td className="flex justify-center gap-2 mt-1 px-6 py-3">
                          <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
                          <Link to="#" title="Ubah" className="text-green-500" onClick={() => handleEdit(savedData.id)}><FaEdit /></Link>
                     </td>
