f0bdc9f677e48419ba21fb445d496fb9b0df98a1
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-11-30 11:13:54 +0700

    Update format date month

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -6,20 +6,25 @@ const DataNoteTable = ({ savedData, handleDelete, handleEdit }) => {
           return null;
      }
 
+     // Format Amount
      const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.nominal);
-     const partDate = savedData.tanggal.split("-");
-     const formattedDate = `${partDate[2]}/${partDate[1]}/${partDate[0]}`;
+
+     // Format Date
+     const formattedDate = new Intl.DateTimeFormat('id-ID', { day: 'numeric', month: 'short', year: 'numeric' }).format(new Date(savedData.tanggal));
+
+     const splitDate = formattedDate.split(' ');
+     const dateSlash = `${splitDate[0]}/${splitDate[1]}/${splitDate[2]}`;
 
      return (
           <>
                <tr className="bg-primary border-gray-300">
                     <td className="whitespace-nowrap px-6 py-3">{savedData.deskripsi}</td>
-                    <td className="px-6 py-3">{formattedDate}</td>
+                    <td className="px-6 py-3">{dateSlash}</td>
                     <td className="px-6 py-3">{formatAmountID}</td>
                     <td className="px-6 py-3">{savedData.jenis === "pemasukan" ? "pemasukan" : "pengeluaran"}</td>
-                    <td className="flex justify-center gap-2 mt-1 px-6 py-3">
+                    <td className="flex justify-center gap-4 mt-1 px-6 py-3">
                          <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
-                         <Link to="#" title="Ubah" className="text-green-500" onClick={() => handleEdit(savedData.id)}><FaEdit /></Link>
+                         <Link to="#" title="Edit" className="text-green-500" onClick={() => handleEdit(savedData.id)}><FaEdit /></Link>
                     </td>
                </tr>
           </>
