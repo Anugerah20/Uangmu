6fc9ff31572021f39dd52f1205e2641e79284e17
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-12-31 13:42:42 +0700

    feat: add login and register to react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,29 @@ import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import LoginCard from "./components/LoginCard";
+import RegisterCard from "./components/RegisterCard";
 
 function App() {
+  const authPage = window.location.pathname === "/login" || window.location.pathname === "/register";
+
   return (
-    <div>
-      <Navigation />
-      <div className="h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/takenotes" element={<TakeNotes />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+    <>
+      {!authPage && <Navigation />}
+      <div>
+        <div className="h-screen">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/takenotes" element={<TakeNotes />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/register" element={<RegisterCard />} />
+            <Route path="/login" element={<LoginCard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        {!authPage && <Footer />}
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
