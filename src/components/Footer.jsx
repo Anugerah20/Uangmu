0afe7dcfb6117efd5d4e6f6e92befa9803720aba
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-12-31 13:46:52 +0700

    styling: update color text footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,16 @@ export default function MyFooter() {
         <Footer.Copyright
           by="Uangmu"
           href="/"
+          className="text-sky-600"
         />
-        <Footer.LinkGroup className="flex justify-end sm:justify-around">
-          <Footer.Link href="/" className="px-1 underline">
+        <Footer.LinkGroup className="flex justify-end sm:justify-around text-sky-600">
+          <Footer.Link href="/" className="px-1">
             Beranda
           </Footer.Link>
-          <Footer.Link href="/takenotes" className="px-1 underline">
+          <Footer.Link href="/takenotes" className="px-1">
             Buat Catatan
           </Footer.Link>
-          <Footer.Link href="/contact" className="px-1 underline">
+          <Footer.Link href="/contact" className="px-1">
             Kontak
           </Footer.Link>
         </Footer.LinkGroup>
