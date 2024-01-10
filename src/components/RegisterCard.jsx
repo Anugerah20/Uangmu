3e4a75584a02eb74c61a7cb8be02e07383312c2d
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-10 21:38:32 +0700

    refactor: make register user

--- src/components/RegisterCard.jsx
+++ src/components/RegisterCard.jsx
@@ -1,6 +1,15 @@
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { Toaster, toast } from "sonner";
+import { userApiPost } from '../services/apiService';
+import { useState } from 'react';
 
 export default function RegisterCard() {
+
+     const [loading, setLoading] = useState(false);
+
+     const navigate = useNavigate();
+
      const {
           register,
           handleSubmit,
@@ -8,16 +17,54 @@ export default function RegisterCard() {
           reset,
      } = useForm();
 
-     const onSubmit = (data) => {
-          console.log(data);
-          reset();
+     const onSubmit = async (data) => {
+          try {
+               setLoading(true);
+
+               const registerData = {
+                    fullname: data.username,
+                    email: data.email,
+                    password: data.password,
+               };
+
+               const response = await userApiPost("/register", registerData);
+               console.log("Data register: ", response);
+
+               if (response && response.data.token) {
+                    localStorage.setItem("tokenUser", response.data.token);
+                    localStorage.setItem("userId", response.data.user.id);
+
+                    toast.success("Register Successfully");
+                    setTimeout(() => {
+                         navigate("/takenotes");
+                    }, 600);
+                    reset();
+               }
+
+          } catch (error) {
+               if (error.message === "Network Error") {
+                    toast.error("Internal Server Error");
+                    console.log("Internal Server Error: ", error);
+                    reset();
+                    return;
+               }
+
+               const duplicateEmail = data.email;
+               if (duplicateEmail) {
+                    toast.error("Email already registered, use another email address");
+                    reset();
+                    return;
+               }
+          } finally {
+               setLoading(false);
+          }
      };
 
      return (
           <div className="h-screen flex justify-center items-center">
                <form onSubmit={handleSubmit(onSubmit)} className="max-w-md lg:w-full md:w-4/5 w-4/5 flex-col gap-4 mx-auto py-10">
                     <h1 className="text-2xl font-bold mb-8">REGISTER <span className="text-sky-500 border-b-2 border-sky-500">UANGMU</span></h1>
-
+                    <Toaster />
                     <div>
                          <div className="mb-1 block">
                               <label htmlFor="username" className="text-sm font-medium">Username</label>
@@ -43,9 +90,10 @@ export default function RegisterCard() {
                          {errors.password && errors.password.type === "minLength" && <span className="text-sm text-red-400">Password min 8 character</span>}
                     </div>
                     <div className="flex items-center gap-2 my-4">
-                         <label htmlFor="login">Do you already have an account?, <a href="/login" className="text-sky-500 font-bold">please login</a></label>
+                         <label htmlFor="login">Do you have an account? <a href="/login" className="text-sky-500 font-bold"> login</a></label>
                     </div>
-                    <button type="submit" className="btn-login">REGISTER</button>
+                    <button type="submit" className="btn-login">{loading ? "Loading..." : "REGISTER"}
+                    </button>
                </form>
           </div>
      );
