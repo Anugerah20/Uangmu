c618ac0eccade87ca4ea030277dc6d2edc6eee4a
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-01-11 20:47:46 +0700

    refactor: structure folder

--- utils/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ authenticated, path, element: Element }) => {
-     return authenticated ? <Element /> : <Navigate to="/login" />
-}
-
-export default PrivateRoute;
