76bb871929acf5cfe1b10d73bd2c76564022cbfe
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-03-23 21:03:18 +0700

    update: show note money

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -2,32 +2,32 @@ import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const DataNoteTable = ({ savedData, handleDelete, handleEdit }) => {
-     if (!savedData) {
-          return null;
-     }
+     // if (!savedData) {
+     //      return null;
+     // }
 
      // Format Amount
-     const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.nominal);
+     const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.price);
 
      // Format Date
-     const formattedDate = savedData.tanggal ? new Intl.DateTimeFormat('id-ID',
+     const formattedDate = savedData.date ? new Intl.DateTimeFormat("id-ID",
           {
-               day: 'numeric',
-               month: 'short',
-               year: 'numeric'
-          }).format(new Date(savedData.tanggal))
+               day: "numeric",
+               month: "short",
+               year: "numeric"
+          }).format(new Date(savedData.date))
           : null;
 
-     const splitDate = formattedDate.split(' ');
+     const splitDate = formattedDate.split(" ");
      const dateSlash = `${splitDate[0]}/${splitDate[1]}/${splitDate[2]}`;
 
      return (
           <>
                <tr className="bg-primary border-gray-300">
-                    <td className="whitespace-nowrap px-6 py-3">{savedData.deskripsi}</td>
+                    <td className="whitespace-nowrap px-6 py-3">{savedData.description}</td>
                     <td className="px-6 py-3">{dateSlash}</td>
                     <td className="px-6 py-3">{formatAmountID}</td>
-                    <td className="px-6 py-3">{savedData.jenis === "pemasukan" ? "pemasukan" : "pengeluaran"}</td>
+                    <td className="px-6 py-3">{savedData.noteType}</td>
                     <td className="flex justify-center gap-4 mt-1 px-6 py-3">
                          {/* <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link> */}
                          <Link to="#" title="Hapus" className="text-red-500"><FaTrashAlt /></Link>
