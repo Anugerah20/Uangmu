a916e5246505ed0ba687229532c6c0ed41d708d5
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-03-30 19:22:08 +0700

    update: consistent codes

--- src/components/FilterMoney.jsx
+++ src/components/FilterMoney.jsx
@@ -1,13 +1,13 @@
 const FilterMoney = ({ selectMonth, setSelectMonth }) => {
      const months = [
-          'Semua Bulan', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli',
-          'Agustus', 'September', 'Oktober', 'November', 'Desember'
+          "Semua Bulan", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli",
+          "Agustus", "September", "Oktober", "November", "Desember"
      ];
 
      return (
           <div>
                {/* <label htmlFor="selectMonth">Filter Bulanan :</label> */}
-               <select className="rounded-md h-10 ml-2 w-full" id="selectMonth" value={selectMonth} onChange={(e) => setSelectMonth(e.target.value)}>
+               <select className="rounded-md h-10 w-full mb-10" id="selectMonth" value={selectMonth} onChange={(e) => setSelectMonth(e.target.value)}>
                     {months.map((month) => (
                          <option key={month} value={month}>
                               {month}
