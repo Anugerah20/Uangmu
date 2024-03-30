4151eb8f6aee3a6d8495288f45e1e769d92a5c88
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-03-30 19:20:58 +0700

    feat: confirm delete note by userId

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -1,10 +1,17 @@
+import { data } from "autoprefixer";
 import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "sonner";
 
-const DataNoteTable = ({ savedData, handleDelete, handleEdit }) => {
-     // if (!savedData) {
-     //      return null;
-     // }
+const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete, handleEdit }) => {
+     const handleDelete = (id) => {
+          const confirmDelete = window.confirm("Apakah Anda yakin ingin menghapus catatan ini?");
+          if (confirmDelete) {
+               onDelete(id);
+               toast.success("Catatan berhasil dihapus");
+               onSubmitSuccess();
+          }
+     }
 
      // Format Amount
      const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.price);
@@ -23,15 +30,15 @@ const DataNoteTable = ({ savedData, handleDelete, handleEdit }) => {
 
      return (
           <>
+
                <tr className="bg-primary border-gray-300">
                     <td className="whitespace-nowrap px-6 py-3">{savedData.description}</td>
                     <td className="px-6 py-3">{dateSlash}</td>
                     <td className="px-6 py-3">{formatAmountID}</td>
                     <td className="px-6 py-3">{savedData.noteType}</td>
                     <td className="flex justify-center gap-4 mt-1 px-6 py-3">
-                         {/* <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link> */}
-                         <Link to="#" title="Hapus" className="text-red-500"><FaTrashAlt /></Link>
-                         <Link to="#" title="Edit" className="text-green-500"><FaEdit /></Link>
+                         <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(data.id)}><FaTrashAlt /></Link>
+
                          {/* <Link to="#" title="Edit" className="text-green-500" onClick={() => handleEdit(savedData.id)}><FaEdit /></Link> */}
                     </td>
                </tr>
