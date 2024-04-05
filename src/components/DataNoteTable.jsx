cc6432986f489e166779cce9a9042158d5b4b20e
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-04-05 16:06:40 +0700

    feat: edit note

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -1,9 +1,13 @@
-import { data } from "autoprefixer";
 import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
+import EditModalNote from "./EditModalNote";
+import { useEffect, useState } from "react";
 
-const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete, handleEdit }) => {
+const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
+     const [isOpenEdit, setIsOpenEdit] = useState(false);
+     const [triggerEffect, setTriggerEffect] = useState(false);
+     // Fungsi konfrimasi hapus data
      const handleDelete = (id) => {
           const confirmDelete = window.confirm("Apakah Anda yakin ingin menghapus catatan ini?");
           if (confirmDelete) {
@@ -23,23 +27,29 @@ const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete, handleEdit }) => 
                month: "short",
                year: "numeric"
           }).format(new Date(savedData.date))
-          : null;
+          : "";
+
+     console.log(formattedDate);
 
      const splitDate = formattedDate.split(" ");
-     const dateSlash = `${splitDate[0]}/${splitDate[1]}/${splitDate[2]}`;
+     const dateSlash = `${splitDate[0]}-${splitDate[1]}-${splitDate[2]}`;
+
+     useEffect(() => {
+          setIsOpenEdit(false);
+     }, [triggerEffect])
 
      return (
           <>
-
+               <EditModalNote date={formattedDate} isOpen={isOpenEdit} data={savedData} onSubmitSuccess={() => setTriggerEffect(!triggerEffect)} />
                <tr className="bg-primary border-gray-300">
                     <td className="whitespace-nowrap px-6 py-3">{savedData.description}</td>
                     <td className="px-6 py-3">{dateSlash}</td>
                     <td className="px-6 py-3">{formatAmountID}</td>
                     <td className="px-6 py-3">{savedData.noteType}</td>
                     <td className="flex justify-center gap-4 mt-1 px-6 py-3">
-                         <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(data.id)}><FaTrashAlt /></Link>
+                         <Link to={`/delete/${savedData.id}`} title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
 
-                         {/* <Link to="#" title="Edit" className="text-green-500" onClick={() => handleEdit(savedData.id)}><FaEdit /></Link> */}
+                         <button title="Edit" className="text-green-500" onClick={() => setIsOpenEdit(!isOpenEdit)}><FaEdit /></button>
                     </td>
                </tr>
           </>
