60283ff4c2a0c0d5597b73cab6adfb87b0f41cb3
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-23 22:07:16 +0700

    update: format date data note and edit note

--- src/services/formatDate.js
+++ src/services/formatDate.js
@@ -1,9 +1,25 @@
-const formatDate = (dateString) => {
+// EDITOR: Nabil (23/05/2024)
+// const formatDate = (dateString) => {
+//      const date = new Date(dateString);
+//      const year = date.getFullYear();
+//      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+//      const day = date.getDate().toString().padStart(2, "0");
+//      return `${year}-${month}-${day}`;
+// }
+// export default formatDate;
+
+const formatDate = (dateString, format = "DD-MM-YYYY") => {
      const date = new Date(dateString);
-     const day = date.getUTCDate();
-     const month = date.getUTCMonth() + 1;
-     const year = date.getUTCFullYear();
-     return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+     const year = date.getFullYear();
+     const month = (date.getMonth() + 1).toString().padStart(2, "0");
+     const day = date.getDate().toString().padStart(2, "0");
+
+     if (format === "DD-MM-YYYY") {
+          return `${day}-${month}-${year}`;
+     } else if (format === "YYYY-MM-DD") {
+          return `${year}-${month}-${day}`;
+     }
+     return dateString;
 }
 
 export default formatDate;
