ea37678ea97327db2b4e3f41fc45e01dea5cc8b2
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-23 22:07:38 +0700

    update: format date note

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import EditModalNote from "./EditModalNote";
 import { useEffect, useState } from "react";
+import formatDate from "../services/formatDate";
 
 const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
-     const [isOpenEdit, setIsOpenEdit] = useState(false);
+     const [isOpenEdit] = useState(false);
      const [triggerEffect, setTriggerEffect] = useState(false);
 
      // Fungsi konfrimasi hapus data
@@ -13,8 +14,8 @@ const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
           const confirmDelete = window.confirm("Apakah Anda yakin ingin menghapus catatan ini?");
           if (confirmDelete) {
                onDelete(id);
-               savedData(savedData.filter((data) => data.id !== id));
                toast.success("Catatan berhasil dihapus");
+               savedData(savedData.filter((data) => data.id !== id));
                onSubmitSuccess();
           }
      }
@@ -22,18 +23,21 @@ const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
      // Format Amount
      const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.price);
 
+     // EDITOR: Nabil (23/05/2024)
      // Format Date
-     const formattedDate = savedData.date ? new Intl.DateTimeFormat("id-ID",
-          {
-               day: "2-digit",
-               month: "2-digit",
-               year: "numeric",
-          }).format(new Date(savedData.date))
-          : "";
+     // const formattedDate = savedData.date ? new Intl.DateTimeFormat("id-ID",
+     //      {
+     //           month: "2-digit",
+     //           day: "2-digit",
+     //           year: "numeric",
+     //      }).format(new Date(savedData.date))
+     //      : "";
 
      // const splitDate = formattedDate.split(" ");
      // const dateSlash = `${splitDate[0]}-${splitDate[1]}-${splitDate[2]}`;
-     const dateSlash = formattedDate.split(" ");
+     // const dateSlash = formattedDate.split(" ");
+
+     const formattedDate = formatDate(savedData.date, 'DD-MM-YYYY');
 
      useEffect(() => {
           onSubmitSuccess();
@@ -43,13 +47,13 @@ const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
           <>
                <tr className="bg-primary border-gray-300">
                     <td className="whitespace-nowrap px-6 py-3">{savedData.description}</td>
-                    <td className="px-6 py-3">{dateSlash}</td>
+                    <td className="px-6 py-3">{formattedDate}</td>
                     <td className="px-6 py-3">{formatAmountID}</td>
                     <td className="px-6 py-3">{savedData.noteType}</td>
                     <td className="flex justify-center gap-4 mt-1 px-6 py-3">
                          <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
 
-                         <EditModalNote date={formattedDate} isOpen={isOpenEdit} data={savedData} onSubmitSuccess={() => setTriggerEffect(!triggerEffect)} />
+                         <EditModalNote date={savedData.date} isOpen={isOpenEdit} data={savedData} onSubmitSuccess={() => setTriggerEffect(!triggerEffect)} />
                     </td>
                </tr >
           </>
