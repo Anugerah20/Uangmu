6eefbe8d72ae15694483d16e1550d004cba45ead
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-05-24 19:34:21 +0700

    update: confirm delete note by id

--- src/components/DataNoteTable.jsx
+++ src/components/DataNoteTable.jsx
@@ -4,38 +4,43 @@ import { toast } from "sonner";
 import EditModalNote from "./EditModalNote";
 import { useEffect, useState } from "react";
 import formatDate from "../services/formatDate";
+import ConfirmDeleteNote from "./ConfirmDeleteNote";
 
 const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
      const [isOpenEdit] = useState(false);
      const [triggerEffect, setTriggerEffect] = useState(false);
+     const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
+     const [deleteId, setDeleteId] = useState(null);
 
      // Fungsi konfrimasi hapus data
      const handleDelete = (id) => {
-          const confirmDelete = window.confirm("Apakah Anda yakin ingin menghapus catatan ini?");
-          if (confirmDelete) {
-               onDelete(id);
-               toast.success("Catatan berhasil dihapus");
-               savedData(savedData.filter((data) => data.id !== id));
-               onSubmitSuccess();
-          }
+          setDeleteId(id);
+          setIsConfirmDeleteOpen(true);
      }
+
+     const confirmDelete = () => {
+          if (deleteId !== null) {
+               onDelete(deleteId);
+               toast.success("Catatan berhasil dihapus");
+               onSubmitSuccess();
+               setIsConfirmDeleteOpen(false);
+               setTriggerEffect(!triggerEffect);
+          }
+     };
+
+     // Fungsi konfrimasi hapus data
+     // const handleDelete = (id) => {
+     //      const confirmDelete = window.confirm("Apakah Anda yakin ingin menghapus catatan ini?");
+     //      if (confirmDelete) {
+     //           onDelete(id);
+     //           toast.success("Catatan berhasil dihapus");
+     //           savedData(savedData.filter((data) => data.id !== id));
+     //           onSubmitSuccess();
+     //      }
+     // }
 
      // Format Amount
      const formatAmountID = new Intl.NumberFormat("id-ID").format(savedData.price);
-
-     // EDITOR: Nabil (23/05/2024)
-     // Format Date
-     // const formattedDate = savedData.date ? new Intl.DateTimeFormat("id-ID",
-     //      {
-     //           month: "2-digit",
-     //           day: "2-digit",
-     //           year: "numeric",
-     //      }).format(new Date(savedData.date))
-     //      : "";
-
-     // const splitDate = formattedDate.split(" ");
-     // const dateSlash = `${splitDate[0]}-${splitDate[1]}-${splitDate[2]}`;
-     // const dateSlash = formattedDate.split(" ");
 
      const formattedDate = formatDate(savedData.date, 'DD-MM-YYYY');
 
@@ -51,11 +56,13 @@ const DataNoteTable = ({ savedData, onSubmitSuccess, onDelete }) => {
                     <td className="px-6 py-3">{formatAmountID}</td>
                     <td className="px-6 py-3">{savedData.noteType}</td>
                     <td className="flex justify-center gap-4 mt-1 px-6 py-3">
-                         <Link to="#" title="Hapus" className="text-red-500" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
+                         <Link to="#" title="Hapus" onClick={() => handleDelete(savedData.id)}><FaTrashAlt /></Link>
 
                          <EditModalNote date={savedData.date} isOpen={isOpenEdit} data={savedData} onSubmitSuccess={() => setTriggerEffect(!triggerEffect)} />
                     </td>
                </tr >
+
+               <ConfirmDeleteNote isOpen={isConfirmDeleteOpen} onClose={() => setIsConfirmDeleteOpen(false)} onConfirm={confirmDelete} />
           </>
      );
 };
