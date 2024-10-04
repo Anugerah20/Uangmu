494a4cb4bdb3597abe80f4219959a80c718f90fd
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-04 11:28:06 +0700

    Refactor TakeNotes component to remove fixed width and height for ChartMoney

--- src/pages/TakeNotes.jsx
+++ src/pages/TakeNotes.jsx
@@ -186,7 +186,7 @@ const TakeNotes = () => {
 
                {/* START: CHART */}
                <div className="flex justify-center flex-col">
-                    <ChartMoney className="w-[400px] h-[300px]" />
+                    <ChartMoney />
                </div>
                {/* END: CHART */}
           </>
