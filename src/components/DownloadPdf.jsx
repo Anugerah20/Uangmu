ced5f574c2fae0b163f78edd8b0cda4ec1d4b17d
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-10-24 11:05:15 +0700

    Refactor DownloadPdf component to fetch financial data and add download functionality

--- src/components/DownloadPdf.jsx
+++ src/components/DownloadPdf.jsx
@@ -1,11 +1,31 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/prop-types */
 import { jsPDF } from "jspdf";
+import { useApiGet } from "../services/apiService";
+import { useEffect, useState } from "react";
 
-const DownloadPdf = ({ financialData, selectMonth }) => {
+const DownloadPdf = ({ selectMonth }) => {
+     const [financialData, setFinancialData] = useState([]);
+
+     useEffect(() => {
+          const fecthData = async () => {
+               try {
+                    const userId = localStorage.getItem("userId");
+                    const response = await useApiGet(`/get-all-note/${userId}`);
+                    setFinancialData(response?.data?.showAllNotes);
+                    console.log("Download PDF: ", setFinancialData);
+               } catch (error) {
+                    console.log(error);
+               }
+          }
+          fecthData();
+     }, []);
+
      const downloadFile = () => {
           const pdf = new jsPDF();
           pdf.text("Laporan Keuangan", 20, 10);
 
-          const filteredData = selectMonth === "Semua Bulan" ? financialData : financialData.filter((data) => new Date(data.tanggal).toLocaleString("id-ID", { month: "long" }) === selectMonth);
+          const filteredData = selectMonth === "Semua Bulan" ? financialData : financialData.filter((data) => new Date(data.date).toLocaleString("id-ID", { month: "long" }) === selectMonth);
 
           // Setting row and column table
           const headerYPos = 40;
@@ -27,26 +47,25 @@ const DownloadPdf = ({ financialData, selectMonth }) => {
 
           filteredData.forEach((data, index) => {
                const yPos = headerYPos + (index + 1) * rowHeight;
-               const formattedNominal = new Intl.NumberFormat("id-ID").format(data.nominal);
+               const formattedNominal = new Intl.NumberFormat("id-ID").format(data.price);
 
-               pdf.text(data.deskripsi, col1XPos, yPos);
-               pdf.text(new Date(data.tanggal).toLocaleDateString("id-ID"), col2XPos, yPos);
+               pdf.text(data.description, col1XPos, yPos);
+               pdf.text(new Date(data.date).toLocaleDateString("id-ID"), col2XPos, yPos);
                pdf.text("Rp " + formattedNominal, col3XPos, yPos);
-               pdf.text(data.jenis === "pemasukan" ? "Pemasukan" : "Pengeluaran", col4XPos, yPos);
+               pdf.text(data.noteType === "pemasukan" ? "Pemasukan" : "Pengeluaran", col4XPos, yPos);
           });
 
           pdf.save("laporan_keuangan.pdf");
      }
 
-     const checkButtonVisible = financialData.length > 0 && (selectMonth === "Semua Bulan" || financialData.some(data => new Date(data.tanggal).toLocaleString("id-ID", { month: "long" }) === selectMonth));
+     const checkButtonVisible = financialData.length > 0 && (selectMonth === "Semua Bulan" || financialData.some(data => new Date(data.date).toLocaleString("id-ID", { month: "long" }) === selectMonth));
 
      return (
           <>   {checkButtonVisible && (
-               // <button className="btn-note lg:my-4 md:my-5 my-5" onClick={downloadFile}>Download PDF</button>
                <button className="btn-note mb-10" onClick={downloadFile}>Download PDF</button>
           )}
           </>
      )
 }
 
-export default DownloadPdf
+export default DownloadPdf;
